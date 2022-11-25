c415e224b7185d96962cc2fec14ceb97c5629a46
carmencampillo <carmencs12@hotmail.com>
2022-11-25 12:38:54 +0100

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useState } from "react"
+import { defaultMethod } from "react-router-dom/dist/dom"
+function Login ({onSubmit}) {
+  const [username, setUsername] = useState ("")
+  const  [password, setPassword] = useState("")
 
-function Login() {
-  return <div>Login</div>;
-}
-
-export default Login;
+  return (
+    <form onSubmit={event => { 
+      event.preventDefault()
+      onSubmit({username, password})
+    }}>
+         <label htmlFor="username">Username</label>
+      <input id="username" value={username} onChange={e => setUsername(e.target.value)} />
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">submit</button>
+    </form>
+  
+    )}
+    export default Login;
