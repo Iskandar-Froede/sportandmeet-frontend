a2b0a0af7188f70c3513f6d7840c3c83403f91af
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-25 14:50:23 +0100

    test3

---
+++ src/pages/Events.jsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+
+function Events() {
+  const { token, fetchWithToken } = useContext(SessionContext);
+
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchEvents = fetchWithToken("GET", "/events", setEvents);
+
+  useEffect(() => {
+    fetchEvents();
+    setIsLoading(false);
+  }, []);
+
+  const deleteEvent = async (eventId) => {
+    await fetch(`http://localhost:5005/api/events/${eventId}`, {
+      method: "DELETE",
+    });
+    fetchBeers();
+  };
+
+  return (
+    <div>
+      <h1>My Evenst</h1>
+      {token}
+      <NewComment fetchEvents={fetchEvents} />
+
+      {events.map((event) => (
+        <Event
+          key={event._id}
+          event={event}
+          deleteEvent={deleteEvent}
+          fetchEvents={fetchEvents}
+          isLoading={isLoading}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Events;
