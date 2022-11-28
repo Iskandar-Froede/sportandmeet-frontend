d3af241587bb69cc0b32455857b6e898aaa5ecc2
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-28 16:55:40 +0100

    update comments

--- src/Components/NewComment.jsx
+++ src/Components/NewComment.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { SessionContext } from "../contexts/Session.Context";
+import { useContext } from "react";
 const API_URL = "http://localhost:5005";
 
 function NewComment(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [created, setCreated] = useState("");
 
-  const { id } = props;
-  const requestBody = { title, description, id };
+  const { user } = useContext(SessionContext);
+
+  const requestBody = { title, description, userId: user._id };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .post(`${API_URL}/events`, requestBody)
+      .post(`${API_URL}/events/comments/${props.eventId}`, requestBody)
       .then((response) => {
+        console.log(response.data);
         // Reset the state to clear the inputs
         setTitle("");
         setDescription("");
-        setCreated("");
 
         // Invoke the callback function coming through the props
-        // from the ProjectDetailsPage, to refresh the project details
-        props.refreshProject();
+
+        props.refreshEvent();
       })
       .catch((error) => console.log(error));
   };
