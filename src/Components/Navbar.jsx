681ef5d42275ab97809e8f699440e6477c656fec
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-30 15:25:06 +0100

    change navbar css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../Styles/Navbar.css";
+import "../Styles/navbar.css";
 
 function Navbar(props) {
   const { authContext } = props;
