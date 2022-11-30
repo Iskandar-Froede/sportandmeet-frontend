26df1c62c99c7a61a736bd3a4966925be08f3187
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-30 09:51:15 +0100

    align with alberto changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,50 +3,33 @@ import { Link } from "react-router-dom";
 import "../Styles/Navbar.css";
 
 function Navbar(props) {
-  const navRef = useRef();
-
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };
-
   const { authContext } = props;
 
   return (
-    <header>
-      <h3>LOGO</h3>
-      <nav>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        {(authContext && (
-          <>
-            <li>
-              <Link to="/profile">PROFILE</Link>
-            </li>
-            <li>
-              <Link to="/events">EVENTS</Link>
-            </li>
-          </>
-        )) || (
-          <>
-            <li>
-              <Link to="/signup">SIGNUP</Link>
-            </li>
-            <li>
-              <Link to="/login">LOGIN</Link>
-            </li>
-          </>
-        )}
-
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
-        </button>
-      </nav>
-
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
-    </header>
+    <nav>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      {(authContext && (
+        <>
+          <li>
+            <Link to="/profile">Profile Page</Link>
+          </li>
+          <li>
+            <Link to="/events">EVENTS</Link>
+          </li>
+        </>
+      )) || (
+        <>
+          <li>
+            <Link to="/signup">SIGNUP</Link>
+          </li>
+          <li>
+            <Link to="/login">LOGIN</Link>
+          </li>
+        </>
+      )}
+    </nav>
   );
 }
 export default Navbar;
