bc41579ad56472dbb9c12be8760ce5667f188110
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-12-02 10:05:26 +0100

    change line homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,8 +6,11 @@ function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to Sport and Meet</h1>
-      <h2> Have you ever want to play any sport but you are <br />
-         new in town and you don’t have friends?</h2>
+      <h2>
+        {" "}
+        Have you ever want to play any sport but you are new in town and you
+        don’t have anyone to play with?
+      </h2>
       <h3>Do you want to register to Sport and Meet? </h3>
       <Link to="/signup">
         <button className="btn-home">REGISTER</button>
